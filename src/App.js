9270a48d5e51f38d02477c44df71bc54f4323433
Usama51 <64430853+Usama51@users.noreply.github.com>
2020-06-10 00:56:58 +0500

    update logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World from react class 2 piaic_sample8.surge.sh
+      Hello World from react class 2 
     </div>
   );
 }
